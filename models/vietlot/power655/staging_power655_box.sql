59252f6289b93f0ec9bf9ef160bdd7f2b0c89667
datnguye <datnguyen.it09@gmail.com>
2022-06-04 09:37:08 +0700

    [skip ci] Add power655 stag moedls

--- models/vietlot/power655/staging_power655_box.sql
+++ models/vietlot/power655/staging_power655_box.sql
@@ -1,2 +1,14 @@
-select  *
+select  {{ dbt_utils.surrogate_key(['box_id']) }} as sk_box
+        ,cast(box_date as {{ dbt_resto.type_date() }}) as box_date
+        ,box_id
+        ,box_name
+        ,box_result_numbers
+        ,cast(try_parse_json(box_result_numbers)[0] as {{ dbt_utils.type_int() }}) as box_result_number_1
+        ,cast(try_parse_json(box_result_numbers)[1] as {{ dbt_utils.type_int() }}) as box_result_number_2
+        ,cast(try_parse_json(box_result_numbers)[2] as {{ dbt_utils.type_int() }}) as box_result_number_3
+        ,cast(try_parse_json(box_result_numbers)[3] as {{ dbt_utils.type_int() }}) as box_result_number_4
+        ,cast(try_parse_json(box_result_numbers)[4] as {{ dbt_utils.type_int() }}) as box_result_number_5
+        ,cast(try_parse_json(box_result_numbers)[5] as {{ dbt_utils.type_int() }}) as box_result_number_6
+        ,cast(try_parse_json(box_result_numbers)[6] as {{ dbt_utils.type_int() }}) as box_result_number_7
+
 from    {{ ref('vietlot_power655_data') }}
--- models/vietlot/power655/staging_power655_box_detail.sql
+++ models/vietlot/power655/staging_power655_box_detail.sql
@@ -1,2 +1,19 @@
-select  *
+{% if target.type == 'snowflake' %}
+
+select  {{ dbt_utils.surrogate_key(['box_id', 'value:prize_name']) }} as sk_box_detail
+        ,box_id
+        ,cast(value:prize_name as {{ dbt_utils.type_string() }}) as prize_name
+        ,cast(value:prize_won as {{ dbt_utils.type_int() }}) as prize_won
+        ,cast(value:prize_value_raw as {{ dbt_utils.type_string() }}) as prize_value_raw
+        ,cast(value:prize_value as {{ dbt_utils.type_float() }}) as prize_value
+
 from    {{ ref('vietlot_power655_data') }}
+        ,lateral flatten(input => try_parse_json(box_results))
+
+{% else %}
+
+select   box_id
+
+from    {{ ref('vietlot_power655_data') }}
+
+{% endif %}
