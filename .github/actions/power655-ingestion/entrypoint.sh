2becd373cbf21c56a944e79013cd79d53c429824
datnguye <datnguyen.it09@gmail.com>
2022-06-18 12:56:30 +0700

    Update WFUpdate Workflow - #ingestion

--- .github/actions/power655-ingestion/entrypoint.sh
+++ .github/actions/power655-ingestion/entrypoint.sh
@@ -3,6 +3,13 @@ set -e
 
 # cd $*
 
+echo "Installing MongoDB package"
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+
+echo "Setup SKIP CI"
 SKIP_CI="[Ingestion+]"
 if [ $INGESTION_ONLY -eq 1 ]
 then
