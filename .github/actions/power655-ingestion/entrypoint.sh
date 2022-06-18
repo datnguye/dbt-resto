c32777215ca57604e745f360911b16e75dccf548
datnguye <datnguyen.it09@gmail.com>
2022-06-18 12:58:25 +0700

    Update Workflow  #ingestion

--- .github/actions/power655-ingestion/entrypoint.sh
+++ .github/actions/power655-ingestion/entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 # cd $*
 
 echo "Installing MongoDB package"
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
+apt-get update
+apt-get install -y mongodb-org
 
 echo "Setup SKIP CI"
 SKIP_CI="[Ingestion+]"
