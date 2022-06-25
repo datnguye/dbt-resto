c13e1bc1cf061c38827ac06fb53b12d2ad265966
datnguye <datnguyen.it09@gmail.com>
2022-06-25 09:27:25 +0700

    Update Workflow  #ingestion

--- .github/actions/power655-ingestion/entrypoint.sh
+++ .github/actions/power655-ingestion/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 
-# cd $*
+apk add --update apt
 
 echo "Installing MongoDB package"
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
-echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 apt-get update
 apt-get install -y mongodb-org
 
