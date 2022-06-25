bd7c091a3b28a9e0f250e64a143704a7f803a789
datnguye <datnguyen.it09@gmail.com>
2022-06-25 09:30:39 +0700

    Update Workflow  #ingestion

--- .github/actions/power655-ingestion/entrypoint.sh
+++ .github/actions/power655-ingestion/entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 set -e
 
-apk add --update apt
-
-echo "Installing MongoDB package"
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
-echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
-apt-get update
-apt-get install -y mongodb-org
+echo 'http://dl-cdn.alpinelinux.org/alpine/v3.6/main' >> /etc/apk/repositories
+echo 'http://dl-cdn.alpinelinux.org/alpine/v3.6/community' >> /etc/apk/repositories
+apk update
+apk add mongodb-tools
 
 echo "Setup SKIP CI"
 SKIP_CI="[Ingestion+]"
