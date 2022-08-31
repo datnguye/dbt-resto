79af23f255398c86d28f94d9a34485d1f5808458
datnguye <datnguyen.it09@gmail.com>
2022-08-31 09:27:59 +0700

    test_column_values_to_match_regex completed

--- macros/generic_test/if_column_value_to_match_regex.sql
+++ macros/generic_test/if_column_value_to_match_regex.sql
@@ -16,3 +16,33 @@
   select * from exceptions
 
 {% endmacro %}
+
+
+{% macro sqlserver__if_column_value_to_match_regex(model, column_name, regex_expr) %}
+
+  select 'NOT SUPPORTED!' where 1 = 0
+
+{% endmacro %}
+
+
+{% macro postgres__if_column_value_to_match_regex(model, column_name, regex_expr) %}
+
+  {% set expression -%}
+    (select array_agg(i) from (select (regexp_matches({{ column_name }}, '{{ regex_expr }}', 'g'))[1] i) as t)::varchar
+  {%- endset %}
+
+  with exceptions as (
+    select  {{ column_name }},
+            {{ expression }} as {{ column_name }}_matched
+
+    from    {{ model }}
+  )
+  select  *
+  from    exceptions
+  where   {{ column_name }} is not null
+    and   (
+                {{ column_name }}_matched is null
+            or  length({{ column_name }}_matched) = 0
+          )
+
+{% endmacro %}
