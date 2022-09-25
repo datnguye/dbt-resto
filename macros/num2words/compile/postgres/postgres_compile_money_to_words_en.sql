0e632e14ae8e637e95b744f3c067798470734bfa
datnguye <datnguyen.it09@gmail.com>
2022-09-25 10:13:45 +0700

    Disable money to words for Postgres

---
+++ macros/num2words/compile/postgres/postgres_compile_money_to_words_en.sql
@@ -0,0 +1,29 @@
+{% macro postgres__compile_money_to_words_en() -%}
+
+create or replace function {{var('num2words_schema', 'dbtresto')}}.MoneyToWords_EN (money double)
+returns varchar
+as
+$$
+declare
+ result varchar = null;
+begin
+	{# if func = '+' then
+		result =  x + y;
+	elsif  func = '-' then
+		result = x - y;
+	elsif  func = '*' then
+	result = x * y;
+	elsif  func = '/' then
+	result = x / y;
+	else
+		raise exception 'Invalid function provided';
+	end if; #}
+
+
+
+	return result;
+end
+$$
+language plpgsql;
+
+{%- endmacro %}
