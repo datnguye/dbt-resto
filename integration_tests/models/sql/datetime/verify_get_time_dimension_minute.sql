cc8d4434009dde5c1d1766d6497fd6a8b5bf7264
datnguye <datnguyen.it09@gmail.com>
2022-09-25 11:44:06 +0700

    get_time_key support postgres

--- integration_tests/models/sql/datetime/verify_get_time_dimension_minute.sql
+++ integration_tests/models/sql/datetime/verify_get_time_dimension_minute.sql
@@ -1,1 +1,7 @@
+{{
+  config(
+    materialized = ('table' if target.type in ['postgres'] else 'view'),
+  )
+}}
+
 {{ dbt_resto.get_time_dimension(level='minute') }}
--- integration_tests/models/sql/datetime/verify_get_time_dimension_second.sql
+++ integration_tests/models/sql/datetime/verify_get_time_dimension_second.sql
@@ -42,6 +42,12 @@
 
 {% else %}
 
+{{
+  config(
+    materialized = ('table' if target.type in ['postgres'] else 'view'),
+  )
+}}
+
 {{ dbt_resto.get_time_dimension(level='second') }}
 
 {% endif %}
