83dd74c5137522a7654c436c35a8553fd9686f54
datnguye <datnguyen.it09@gmail.com>
2023-05-03 15:38:28 +0700

    feat: #20 add get_data

--- integration_tests/jobs/send_forecast.py
+++ integration_tests/jobs/send_forecast.py
@@ -1,42 +1,49 @@
-import sendgrid
 import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import pyodbc
 
 
 def send_email(
-    sent_from='service@dbt-resto.com',
-    subject='[Daily Forecast] Power 6x55',
-    body='This is a testing or in-completed stuff',
-    debug=False
-  ):
-    tos = os.environ.get('SENDGRID_MAILING_LIST','').split(';')
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY',''))
-    data = {
-      'personalizations': [
-        {
-          'to': tos,
-          'subject': subject
-        }
-      ],
-      'from': {
-        'email': sent_from
-      },
-      'content': [
-        {
-          'type': 'text/html',
-          'value': f'<p>{body}</p>'
-        }
-      ]
-    }
-
-    response = sg.client.mail.send.post(request_body=data)
-    print(f'Sent with response code: {response.status_code}')
-    print(f'Sent with response body: {response.body}')
-    print(f'Sent with response header: {response.headers}')
+    sent_from="service@dbt-resto.com",
+    subject="[Daily Forecast] Power 6x55",
+    body="This is a testing or in-completed stuff",
+):
+    tos = os.environ.get("SENDGRID_MAILING_LIST", "")
+    message = Mail(
+        from_email=sent_from,
+        to_emails=tos,
+        subject=subject,
+        html_content=f"<strong>{body}</strong>",
+    )
+    try:
+        sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
 
 
 def get_data():
-  return 'This is a testing or in-completed stuff'
+    cnxn = pyodbc.connect(
+        "Driver={ODBC Driver 18 for SQL Server};"
+        f"Server={os.environ.get('SQLSERVER_HOST')},{os.environ.get('SQLSERVER_PORT',1433)};"
+        f"Database={os.environ.get('SQLSERVER_DATABASE')};"
+        f"UID={os.environ.get('SQLSERVER_USER')};"
+        f"PWD={os.environ.get('SQLSERVER_PASSWORD')};"
+    )
+    cursor = cnxn.cursor()
+    try:
+        cursor.execute("select * from mart.fact_number_forecast")
+        for row in cursor:
+            return row["forecast_numbers"]
+    except Exception as e:
+        print(e)
+
+    return "ERROR"
 
 
 body = get_data()
-send_email(subject='[power6x55_crawler] SUCCESS to load', body=body)
+send_email(subject="[power6x55_crawler] SUCCESS to load", body=body)
